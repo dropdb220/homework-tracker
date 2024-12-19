f83bbc64c28545d873bffd8ecf2ad90188ee3196
dropdb220 <54498184+dropdb220@users.noreply.github.com>
2024-12-19 11:55:14 +0900

    fix korean font

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" style={{ fontFamily: `${LilitaOne.style.fontFamily}, -apple-system, BlinkMacSystemFont, Apple SD Gothic Neo, Malgun Gothic, sans-serif;` }}>
+    <html lang="ko" style={{ fontFamily: `${LilitaOne.style.fontFamily}, ${Pretendard.style.fontFamily}, -apple-system, BlinkMacSystemFont, "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", sans-serif;` }}>
       <body>
         <Header />
         <main className="w-[90%] mt-4 ml-auto mr-auto">
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-v74';
+const CACHE_NAME = 'cache-v75';
 const UPLOAD_PERMANENT_CACHE_NAME = 'upload-cache';
 
 self.addEventListener('install', event => {
